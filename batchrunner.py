64380b1acf614a6ff7078bcc309c4adb0cbfde5d
Johan van de Glind <vdglind@gmail.com>
2021-12-20 21:51:19 +0100

    added 500 its by 200 steps

--- batchrunner.py
+++ batchrunner.py
@@ -12,20 +12,20 @@ from scipy.stats import norm
 import statistics
 
 model_params = {
-    "car_amount": [1,5,10,15,30,60],
+    "car_amount": [10,15,20,25,30,60],
     "speeding_percentage": [1,5,10,15,20,25,30,35,40,45,50,55,60],
-    "maximum_speeding": [1,2,3,5],
 }
 
 fixed_params = {
     "height": 100,
     "width": 100,
     "brake_prob": 0.1,
+    "maximum_speeding": 3,
 }
 
-param_run = BatchRunner(CarModel,model_params,fixed_params, iterations=1000, max_steps=200,model_reporters={"datacollector": lambda model: model.datacollector.get_model_vars_dataframe()['Throughput'].to_dict()})
+param_run = BatchRunner(CarModel,model_params,fixed_params, iterations=500, max_steps=200,model_reporters={"datacollector": lambda model: model.datacollector.get_model_vars_dataframe()['Throughput'].to_dict()})
 
 param_run.run_all()
 df = param_run.get_model_vars_dataframe()
 print(df)
-df.to_csv('./data_20x500.csv')
+df.to_csv('./data_500x200.csv')
